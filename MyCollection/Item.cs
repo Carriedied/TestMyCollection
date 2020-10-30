cd7cd9c9017fcfd64b8d573ea8ceeb4867988ed0
Carriedied <zherebnenko0202@mail.ru>
2020-10-30 19:25:05 +0700

    Добавьте файлы проекта.

---
+++ MyCollection/Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollection
+{
+    class Item<TId, TName, TValue>
+    {
+        public TId KeyId { get; set; }
+        public TName KeyName { get; set; }
+        public TValue Value { get; set; }
+
+        public Item(TId keyId, TName keyName, TValue value)
+        {
+            if (keyId == null)
+                throw new ArgumentNullException(nameof(keyId));
+
+            if (keyName == null)
+                throw new ArgumentNullException(nameof(keyName));
+
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            KeyId = keyId;
+            KeyName = keyName;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ MyCollection/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var col = new Collection<string, string, string>();
+
+            col.Add("1", "Дмитрий", "Студент");
+            col.Add("2", "Александр", "Студент");
+            col.Add("3", "Артем", "Студент");
+            col.Add("4", "Данил", "Преподаватель");
+            ShowMap(col, "Cleared map");
+            Console.ReadLine();
+        }
+
+        private static void ShowMap(Collection<string, string, string> col, string title)
+        {
+            Console.WriteLine($"{title}: ");
+            foreach (var keyId in col.KeysId)
+            {
+                // Получаем значение словаря по ключу.
+                var value = col.GetById(keyId);
+                Console.WriteLine($"{keyId} - {value}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
